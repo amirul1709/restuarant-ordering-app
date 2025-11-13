c59cd1c2fa4ce5c0ae4e3efa800f7514d0754111
Amirul Hossain <amirul1709@hotmail.com>
2025-11-13 14:45:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ function getMenuItems () {
     const items = menuArray.map((item) => {
        const { name, ingredients, price, emoji, id} = item
         return `
-            <div class="menu-item">
-                <div class="menu-item-info">
-                    <p class="menu-item-emoji">${emoji}</p>
-                        <div>
-                            <p class="menu-item-name">${name}</p>
-                            <p class="menu-item-ingredients">${ingredients.join(', ')}</p>
-                            <p class="menu-item-price">$${price}</p>
-                        </div>
+                <div class="menu-item">
+                    <div class="menu-item-info">
+                        <p class="menu-item-emoji">${emoji}</p>
+                            <div>
+                                <p class="menu-item-name">${name}</p>
+                                <p class="menu-item-ingredients">${ingredients.join(', ')}</p>
+                                <p class="menu-item-price">$${price}</p>
+                            </div>
+                    </div>
+                    <button class="menu-item-add-btn">+</button>
                 </div>
-                <button class="menu-item-add-btn">+</button>
-            </div>
-        `
+                `
     })
     return items.join('')
 }
 
-function addMenuItems () { menuItems.innerHTML = getMenuItems() }
+function addMenuItems () {
+    menuItems.innerHTML = getMenuItems()
+}
 
 addMenuItems()
